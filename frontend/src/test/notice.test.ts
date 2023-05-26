7f2572c8b613e0198e7b32676c98974bbb64f44f
chriseuristic <eanair@kakao.com>
2023-05-27 00:07:03 +0900

    안쓰는 코드 삭제

--- frontend/src/test/notice.test.ts
+++ frontend/src/test/notice.test.ts
@@ -29,24 +29,6 @@ describe('DB Connection Test', () => {
   });
 });
 
-// describe('Notice Test', () => {
-//   it('should fetch and insert notice 100 times', async () => {
-//     for (let i = 0; i < 100; i++) {
-//       mockExecute.mockResolvedValueOnce([{ title: 'test', content: 'test' }, null]);
-
-//       const notices = await getNoticeAll();
-//       // expect(notices).toHaveLength(1);
-//       mockExecute.mockResolvedValueOnce([[{ title: 'test', content: 'test' }], null]);
-//       expect(notices[0]).toEqual({ title: 'test', content: 'test' });
-
-//       const randomTitle = getRandomString(10);
-//       const randomContent = getRandomString(50);
-//       await addNoticePosting({ title: randomTitle, content: randomContent });
-//       expect(mockExecute).toBeCalledWith(`INSERT INTO NOTICE(\`title\`,\`content\`,\`writer\`) VALUES('${randomTitle}','${randomContent}','admin')`);
-//     }
-//   });
-// });
-
 describe('Notice Test', () => {
   it('should fetch and insert notice 100 times', async () => {
     for (let i = 0; i < 100; i++) {
