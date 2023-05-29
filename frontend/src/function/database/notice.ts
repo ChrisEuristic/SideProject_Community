0a3764a8af4665b94cf173bd2438fee081fd3435
chriseuristic <eanair@kakao.com>
2023-05-29 12:14:59 +0900

    기능 추가 완료

--- frontend/src/function/database/notice.ts
+++ frontend/src/function/database/notice.ts
@@ -3,7 +3,10 @@ import { Connection, createConnection } from "mysql2/promise";
 
 export async function getConnection() {
   return await createConnection({
-    host: process.env.ENVIRONMENT === "production" ? process.env.DB_HOST_PRD : process.env.DB_HOST_DEV,
+    host:
+      process.env.ENVIRONMENT === "production"
+        ? process.env.DB_HOST_PRD
+        : process.env.DB_HOST_DEV,
     port: 3306,
     user: process.env.DB_ID,
     password: process.env.DB_PW,
@@ -15,24 +18,43 @@ export async function killConnection(connection: Connection) {
   await connection.end();
 }
 
-// export async function getNoticeQuery(){}
-
-export async function getNoticeAll(){
+export async function getNoticeOne(noticeId: string) {
   const connection = await getConnection();
 
-  const [rows, field] = await connection.execute<RowDataPacket[]>("SELECT * FROM NOTICE ORDER BY ID DESC LIMIT 10");
-  
+  const [rows, field] = await connection.execute<RowDataPacket[]>(
+    `SELECT * FROM NOTICE WHERE ID = '${noticeId}'`
+  );
+
   killConnection(connection);
 
-
-  return rows;
-  
+  return rows[0];
 }
 
-export async function addNoticePosting(value: { title: string, content: string }) {
+export async function getNoticeAll() {
   const connection = await getConnection();
 
-  await connection.execute<RowDataPacket[]>("INSERT INTO NOTICE(`title`,`content`,`writer`) VALUES('" + value.title + "','"+ value.content +"','admin')");
-  
+  const [rows, field] = await connection.execute<RowDataPacket[]>(
+    "SELECT * FROM NOTICE ORDER BY ID DESC LIMIT 10"
+  );
+
+  killConnection(connection);
+
+  return rows;
+}
+
+export async function addNoticePosting(value: {
+  title: string;
+  content: string;
+}) {
+  const connection = await getConnection();
+
+  await connection.execute<RowDataPacket[]>(
+    "INSERT INTO NOTICE(`title`,`content`,`writer`) VALUES('" +
+      value.title +
+      "','" +
+      value.content +
+      "','admin')"
+  );
+
   killConnection(connection);
 }
