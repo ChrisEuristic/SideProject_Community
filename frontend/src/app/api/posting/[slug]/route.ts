2a4c2bec4c31536dde516ff13f1a4a9ccabb6c85
chriseuristic <eanair@kakao.com>
2023-06-18 13:22:00 +0900

    MySQL 연동코드 삭제, TODO: 서버리스를 백엔드로 수정 목표설정

--- frontend/src/app/api/posting/[slug]/route.ts
+++ frontend/src/app/api/posting/[slug]/route.ts
@@ -1,34 +1,4 @@
-// import { deleteNoticePosting, getNoticeOne, incrementNoticeVisit } from "@/function/database/mysql";
-// import { getURLInform } from "@/function/util/server";
-// import { NextResponse } from "next/server";
-
-// export async function GET(request: Request) {
-//   const getTarget = getURLInform(request.url).postingNo;
-
-//   try {
-//     incrementNoticeVisit(getTarget);
-//     const result = await getNoticeOne(getTarget as string);
-//     return new NextResponse(JSON.stringify(result), { status: 200 });
-//   } catch (error) {
-//     console.error("게시물 불러오는 중 에러", error);
-//     return new NextResponse("error", { status: 400 });
-//   }
-// }
-
-// export async function DELETE(request: Request) {
-//   const deleteTarget = getURLInform(request.url).postingNo;
-
-//   try {
-//     await deleteNoticePosting(deleteTarget as string);
-//     return new NextResponse("정상 삭제 완료", {status: 200})
-//   } catch (error) {
-//     console.error("/api/posting DELETE request Error");
-//     console.error(error);
-//     return new NextResponse("정상 삭제 완료", {status: 400})
-
-//   }
-// }
-
+// TODO: 서버리스로 진행되던 DB Access 부분 전부 Backend로 변경.
 
 import { deleteNoticePosting, getNoticeOne, incrementNoticeVisit } from "@/function/database/postgres";
 import { getURLInform } from "@/function/util/server";
--- frontend/src/app/api/posting/route.ts
+++ frontend/src/app/api/posting/route.ts
@@ -1,43 +1,4 @@
-// import {
-//   addNoticePosting,
-//   getNoticeAll,
-//   updateNoticePosting,
-// } from "@/function/database/mysql";
-// import { RowDataPacket } from "mysql2";
-// import { NextResponse } from "next/server";
-
-// export async function GET(request: Request) {
-//   const pageNo: string = (request.url.split("?")[1]).split("=")[1];
-//   const content: RowDataPacket[][] = await getNoticeAll(pageNo);
-//   return new NextResponse(JSON.stringify(content), {
-//     status: 200,
-//   })
-// }
-
-// export async function POST(request: Request) {
-//   const reader = request.body?.pipeThrough(new TextDecoderStream()).getReader();
-//   const inReader = await reader?.read();
-//   const { done, value } = inReader ?? { done: true, value: null };
-
-  
-//   addNoticePosting(JSON.parse(value as string));
-//   return new NextResponse("공지사항 작성완료", {
-//     status: 200,
-//   })
-// }
-
-// export async function PUT(request: Request) {
-//   const reader = request.body?.pipeThrough(new TextDecoderStream()).getReader();
-//   const inReader = await reader?.read();
-//   const { done, value } = inReader ?? { done: true, value: null };
-  
-
-//   updateNoticePosting(JSON.parse(value as string));
-  
-//   return new NextResponse("공지사항 수정완료", {
-//     status: 200,
-//   })
-// }
+// TODO: 서버리스로 진행되던 DB Access 부분 전부 Backend로 변경.
 
 import { addNoticePosting, getNoticeAll, updateNoticePosting } from "@/function/database/postgres";
 import { NextResponse } from "next/server";
