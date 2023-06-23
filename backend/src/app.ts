e6ca25ec6c768207fd217256ef79da768fd398c5
chriseuristic <eanair@kakao.com>
2023-06-24 01:31:46 +0900

    aws 테스트 코드

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,8 +33,8 @@ const httpServer = http.createServer(app);
 //   console.log("1. 해당 서버는 피드, 채팅 서버로 다룬다.");
 // });
 
-const server = httpServer.listen(80, () => {
-  console.log("Server is running on port 80");
+const server = httpServer.listen(8086, () => {
+  console.log("Server is running on port 8086");
   console.log("1. This server is Feed & Chat server.");
 });
 
