2548dc4b03fc37e718f60cf32c7d559d4ce6bc00
chriseuristic <eanair@kakao.com>
2023-07-05 17:33:22 +0900

    각 지표 계산 폼 만들기

--- frontend/src/app/test/components/SlideBar.tsx
+++ frontend/src/app/test/components/SlideBar.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { Dispatch, SetStateAction } from "react";
 import styles from "./sliderbar.module.css";
 
-export default function SlideBar() {
-  const handlingSlider = (slider: EventTarget & HTMLInputElement, sliderValue: number) => {
+export default function SlideBar({ setValue }: { setValue: Dispatch<SetStateAction<number>> }) {
+  const handlingSlider = (
+    slider: EventTarget & HTMLInputElement,
+    sliderValue: number
+  ) => {
     /**
      * 오차범위
      */
     const rangeError = -((sliderValue - 50) * 0.04);
-    slider.style.background = `linear-gradient(to right, #28282870 0%, #28282870 ${sliderValue + rangeError}%, rgb(236, 236, 236) ${sliderValue + rangeError}%, rgb(236, 236, 236) 100%)`;
-  }
-
+    slider.style.background = `linear-gradient(to right, #28282870 0%, #28282870 ${
+      sliderValue + rangeError
+    }%, rgb(236, 236, 236) ${
+      sliderValue + rangeError
+    }%, rgb(236, 236, 236) 100%)`;
+    setValue(sliderValue)
+  };
 
   return (
     <>
@@ -19,7 +27,7 @@ export default function SlideBar() {
         className={styles.range}
         max={100}
         onChange={(e) => {
-          handlingSlider(e.target, Number(e.target.value))
+          handlingSlider(e.target, Number(e.target.value));
         }}
       />
     </>
--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -3,8 +3,24 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
 import SlideBar from "./components/SlideBar";
+import {useEffect, useRef, useState} from "react";
 
 export default function TestPage() {
+
+  const [E, setE] = useState(50);
+  const [S, setS] = useState(50);
+  const [T, setT] = useState(50);
+  const [J, setJ] = useState(50);
+  const [A, setA] = useState(50);
+
+  useEffect(() => {
+    console.debug(`E : ${E}`);
+    console.debug(`S : ${S}`);
+    console.debug(`T : ${T}`);
+    console.debug(`J : ${J}`);
+    console.debug(`A : ${A}`);
+  }, [E, S, T, J, A])
+
   return (
     <>
       <main className={styles.main}>
@@ -12,7 +28,9 @@ export default function TestPage() {
           성격 테스트로 자신의 성향과 행동에 대해 정확히 분석 받아보세요!
         </h1>
         <section className={styles.section}>
-          <article className={styles.description}>나에 대해 알아볼까요?</article>
+          <article className={styles.description}>
+            나에 대해 알아볼까요?
+          </article>
           <article>
             <Link href={"/test/personal"} className={styles.link1}>
               테스트 하러가기
@@ -20,7 +38,12 @@ export default function TestPage() {
           </article>
         </section>
         <section className={styles.section}>
-          <article className={styles.description}>연인, 또는 누군가와의 궁합을 알아볼까요? <span className={styles.tip}>두 사람의 테스트 결과로 궁합을 분석해볼 수 있어요.</span></article>
+          <article className={styles.description}>
+            연인, 또는 누군가와의 궁합을 알아볼까요?{" "}
+            <span className={styles.tip}>
+              두 사람의 테스트 결과로 궁합을 분석해볼 수 있어요.
+            </span>
+          </article>
           <article>
             <Link href={"/test/couple"} className={styles.link2}>
               테스트 하러가기
@@ -28,15 +51,27 @@ export default function TestPage() {
           </article>
         </section>
         <section className={styles.section}>
-          <article className={styles.description}>유사한 상대를 만들어 조언을 구해볼까요? <span className={styles.tip}>평소 대하기 어려웠던 사람과 가상으로 대화해볼 수 있어요.</span></article>
+          <article className={styles.description}>
+            유사한 상대를 만들어 조언을 구해볼까요?{" "}
+            <span className={styles.tip}>
+              평소 대하기 어려웠던 사람과 가상으로 대화해볼 수 있어요.
+            </span>
+          </article>
           <article>
             <Link href={"/test/advice"} className={styles.link3}>
               어드바이저 만들기
             </Link>
           </article>
         </section>
-        <SlideBar />
-        <SlideBar />
+        <SlideBar setValue={setE} />
+        <div></div>
+        <SlideBar setValue={setS} />
+        <div></div>
+        <SlideBar setValue={setT} />
+        <div></div>
+        <SlideBar setValue={setJ} />
+        <div></div>
+        <SlideBar setValue={setA} />
       </main>
     </>
   );
